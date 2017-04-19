5f4081ca9e09363e3c347fbd85d28a0aa7f599f0
Ugrasenan Vellai <uvellaichamy@gmail.com>
2017-04-19 17:20:02 +0530

    Add files via upload

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,1 +1,14 @@
-contact.ts
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
